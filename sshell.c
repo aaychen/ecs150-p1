028b1c12f422ec296abca43b48485b0ed8d47e15
Tien Ly <tntly@ucdavis.edu>
2022-01-11 19:35:24 -0800

    finished phrase 2 (maybe)

--- sshell.c
+++ sshell.c
@@ -5,11 +5,19 @@
 #include <sys/wait.h>
 
 #define CMDLINE_MAX 512
+#define ARG_MAX 16
+#define TOKEN_MAX 32
 
-int main(void)
-{
-        char cmd[CMDLINE_MAX];
-        char *args[] = {cmd, NULL};
+
+typedef struct cmdline {
+        char *cmd;
+        char *args[ARG_MAX];
+} cmdline;
+
+int main(void) {
+        cmdline c;
+        char cmdline[CMDLINE_MAX];
+        char *exec_args[ARG_MAX + 2];
 
         while (1) {
                 char *nl;
@@ -20,37 +28,55 @@ int main(void)
                 fflush(stdout);
 
                 /* Get command line */
-                fgets(cmd, CMDLINE_MAX, stdin);
+                fgets(cmdline, CMDLINE_MAX, stdin);
 
                 /* Print command line if stdin is not provided by terminal */
                 if (!isatty(STDIN_FILENO)) {
-                        printf("%s", cmd);
+                        printf("%s", cmdline);
                         fflush(stdout);
                 }
 
                 /* Remove trailing newline from command line */
-                nl = strchr(cmd, '\n');
+                nl = strchr(cmdline, '\n');
                 if (nl)
                         *nl = '\0';
 
+                /* Parse command line */
+                /* Get command */
+                char *token;
+                int i = 0;
+                token = strtok(cmdline, " ");
+                if (token != NULL) {
+                        c.cmd = exec_args[i++] = token;
+                        token = strtok(NULL, " ");
+                }
+                /* Get all arguments */
+                while (token != NULL) {
+                        exec_args[i++] = token;
+                        // c.args[i++] = token;
+                        token = strtok(NULL, " ");
+                }
+                exec_args[i] = NULL;
+
                 /* Builtin command */
-                if (!strcmp(cmd, "exit")) {
+                if (!strcmp(c.cmd, "exit")) {
                         fprintf(stderr, "Bye...\n");
                         break;
                 }
 
                 /* Regular command */
-                // retval = system(cmd);
                 int status;
-                if (!fork()) {
-                        execvp(cmd, args);
+                if (!fork()) { /* The shell creates a child process */
+                        /* The child process runs the command line */
+                        execvp(c.cmd, exec_args);
                         perror("execvp");
                         exit(1);
                 } else {
+                        /* The parent process (the shell) waits for the child process's completion & collects its exit status */
                         waitpid(-1, &status, 0);
                         retval = WEXITSTATUS(status);
                 }
-                fprintf(stderr, "+ completed '%s' [%d]\n", cmd, retval);
+                fprintf(stderr, "+ completed '%s' [%d]\n", cmdline, retval);
         }
 
         return EXIT_SUCCESS;
